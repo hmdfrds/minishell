7fd1e99b47d3a6d839d8cb72a4358081d5e208e5
Firdaus <firdaus1337@gmail.com>
2025-04-10 22:57:50 +0800

    run available command

--- shell.py
+++ shell.py
@@ -8,12 +8,19 @@ while True:
         command_line = input()
 
         args = command_line.split()
-        
+
         if not args:
             continue
-        
+
         if args[0] == "exit":
             break
+
+        try:
+            subprocess.run(args, check=False, shell=True)
+        except FileNotFoundError:
+            print(f"minishell: command not found: {args[0]}")
+        except Exception as e:
+            print(f"minishell: error executing command: {e}")
 
     except EOFError:
         break
