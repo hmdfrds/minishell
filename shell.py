dd568d7a94bc29ae2e0a89d3dea5f3dc7c4678b3
Firdaus <firdaus1337@gmail.com>
2025-04-10 22:47:12 +0800

    handle empty and exit commands

--- shell.py
+++ shell.py
@@ -6,6 +6,15 @@ while True:
     sys.stdout.flush()
     try:
         command_line = input()
+
+        args = command_line.split()
+        
+        if not args:
+            continue
+        
+        if args[0] == "exit":
+            break
+
     except EOFError:
         break
 
